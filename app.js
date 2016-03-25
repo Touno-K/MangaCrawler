0252ef100276cca8738e0a916c9eda78cf20742a
Kananek T <info.dvgamer@gmail.com>
2016-03-25 15:32:20 +0700

    commit on Fri 03/25/2016 15:32:20.13

---
+++ app.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+
+
+var options = {
+  host: 'www.niceoppai.net',
+  path: '/onepiece/800/',
+  port: 80,
+  method: 'GET',
+  headers: {
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language':'th-TH,th;q=0.8,en-US;q=0.6,en;q=0.4,ja;q=0.2',
+    'Cache-Control':'max-age=0',
+    'Connection':'keep-alive',
+    'Host':'www.niceoppai.net',
+    'If-Modified-Since':'Fri, 25 Mar 2016 05:03:04 GMT',
+    'Upgrade-Insecure-Requests':'1',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'
+  }
+}
+var request = http.request(options, function (res) {
+  var data = '';
+  res.on('data', function (chunk) {
+    data += chunk;
+  });
+  res.on('end', function () {
+
+    fs.appendFile('./chunk/onepiece-800.txt', data, (err) => {
+
+    });
+
+    var manga = data.match(/<option value="(.*?)" >(.*?)<\/option>/ig);
+    console.log(manga[3]);
+    // fs.readFile('./chunk/onepiece-800.txt', (err, data) => {
+    //   if (err) throw err;
+    //   console.log(data);
+    // });
+  });
+});
+request.on('error', function (e) {
+    console.log(e.message);
+});
+request.end();
